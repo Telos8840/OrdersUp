d28031c3a8fa9e4daa8cb7f555ce521f7182654f
Saul Guardado <saul.guardado@internetbrands.com>
2014-12-18 17:03:37 -0800

    Updated side menu for beta 14

---
+++ www/app/home/home-ctrl.js
@@ -0,0 +1,13 @@
+/**
+ * Created by sguardado on 12/18/2014.
+ */
+(function () {
+  'use strict';
+
+  angular.module('ordersUpApp').controller('homeCtrl', [homeCtrl]);
+
+  function homeCtrl(){
+
+  };
+
+})();
